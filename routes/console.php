85f85db6f3f2418c2b18234c674e3c012aed96a1
f0ursecond <2ez4second@gmail.com>
2026-08-19 17:00:39 +0700

    Revert rent:reminder schedule to daily

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('rent:reminder')->everyMinute();
+Schedule::command('rent:reminder')->daily();
